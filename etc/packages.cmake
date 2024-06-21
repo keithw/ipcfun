3cbf90f35c6f534abef13eda4046319e41affe8e
Keith Winstein <keithw@cs.stanford.edu>
2024-06-21 12:13:10 -0700

    First benchmark (but too many syscalls)

--- etc/packages.cmake
+++ etc/packages.cmake
@@ -1,4 +1,8 @@
 find_package(PkgConfig REQUIRED)
 pkg_check_modules(papi REQUIRED papi)
-include_directories( ${papi_INCLUDE_DIRS} )
-add_compile_options( ${papi_CFLAGS} )
+include_directories(${papi_INCLUDE_DIRS})
+add_compile_options(${papi_CFLAGS})
+
+pkg_check_modules(Eigen REQUIRED IMPORTED_TARGET GLOBAL eigen3)
+include_directories(SYSTEM ${Eigen_INCLUDE_DIRS})
+add_compile_options(${Eigen_CFLAGS})
--- src/apps/ipcfun.cc
+++ src/apps/ipcfun.cc
@@ -1,5 +1,12 @@
-#include "papi.h"
+#include <papi.h>
+#include <sys/mman.h>
+
+#include <Eigen/Dense>
+#include <cstdlib>
+#include <iostream>
 #include <stdexcept>
+#include <string>
+#include <vector>
 
 using namespace std;
 
@@ -15,10 +22,72 @@ inline void CheckPAPICall( const char* attempt, int ret )
   }
 }
 
-int main( void )
+using MyMatrix = Eigen::Matrix<double, 256, 256>;
+
+inline float instructions_per_cycle()
 {
   float real_time, proc_time, ipc;
   long long ins;
 
   CheckPAPICall( "PAPI_inc", PAPI_ipc( &real_time, &proc_time, &ins, &ipc ) );
+
+  return ipc;
+}
+
+class tagged_error : public std::system_error
+{
+private:
+  std::string attempt_and_error_;
+  int error_code_;
+
+public:
+  tagged_error( const std::error_category& category, const std::string_view s_attempt, const int error_code )
+    : system_error( error_code, category )
+    , attempt_and_error_( std::string( s_attempt ) + ": " + std::system_error::what() )
+    , error_code_( error_code )
+  {}
+
+  const char* what() const noexcept override { return attempt_and_error_.c_str(); }
+
+  int error_code() const { return error_code_; }
+};
+
+int CheckSystemCall( const char* attempt, int ret )
+{
+  if ( ret < 0 ) {
+    throw tagged_error( system_category(), attempt, errno );
+  }
+  return ret;
+}
+
+int main()
+{
+  ios::sync_with_stdio( false );
+
+  // Open dummy file
+  int fd = memfd_create( "dummy", 0 );
+  if ( fd < 0 ) {
+    throw runtime_error( "memfd_create" );
+  }
+
+  // Initialize matrices
+  vector<MyMatrix> matrices( 3 );
+  matrices[1].Random();
+  matrices[2].Random();
+
+  vector<float> ipc_measurements( 4000 );
+
+  for ( int i = 0; i < 2000; ++i ) {
+    ipc_measurements.at( i ) = instructions_per_cycle();
+    matrices[0] = matrices[1] * matrices[2];
+  }
+
+  CheckSystemCall( "pwrite", pwrite( fd, nullptr, 0, 0 ) );
+
+  for ( int i = 2000; i < 4000; ++i ) {
+    ipc_measurements.at( i ) = instructions_per_cycle();
+    matrices[0] = matrices[1] * matrices[2];
+  }
+
+  return EXIT_SUCCESS;
 }
