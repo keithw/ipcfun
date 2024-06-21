54a81838af4dbbf736906dd765fef106e6ea82c5
Keith Winstein <keithw@cs.stanford.edu>
2024-06-21 16:13:04 -0700

    Plot boxes for each interval

--- src/ipcfun.cc
+++ src/ipcfun.cc
@@ -9,8 +9,13 @@
 
 using namespace std;
 
-constexpr unsigned int total_iterations = 4000;
-constexpr unsigned int system_call_at = 2000;
+constexpr unsigned int total_iterations = 100000;
+constexpr unsigned int system_call_at = total_iterations / 2;
+
+struct SamplePair
+{
+  IPCCounter::Reading pre, post;
+};
 
 int main()
 {
@@ -32,36 +37,43 @@ int main()
 
   // Initialize IPC computation
   IPCCounter perf;
-  vector<IPCCounter::Reading> samples( total_iterations );
+  vector<SamplePair> samples( total_iterations );
   perf.start();
 
-  // Do computation before the system call
-  for ( unsigned int i = 0; i < system_call_at; ++i ) {
-    samples.at( i ) = perf.read();
-    matrices[0] = matrices[1] * matrices[2];
+  // In each iteration, do computation or a system call
+  for ( unsigned int i = 0; i < total_iterations; ++i ) {
+    const auto sample = perf.read();
+    samples.at( i ).pre = sample;
+    if ( i > 0 ) {
+      samples.at( i - 1 ).post = sample;
+    }
+
+    if ( i == system_call_at ) { // do system call in this iteration (not printed)
+      CheckSystemCall( "pwrite", pwrite( fd, nullptr, 0, 0 ) );
+    } else { // otherwise, do computation and print stats
+      matrices[0] = matrices[1] * matrices[2];
+    }
   }
 
-  // Make system call
-  CheckSystemCall( "pwrite", pwrite( fd, nullptr, 0, 0 ) );
-
-  // Do computation after the system call
-  for ( unsigned int i = system_call_at; i < total_iterations; ++i ) {
-    samples.at( i ) = perf.read();
-    matrices[0] = matrices[1] * matrices[2];
-  }
+  samples.back().post = perf.read(); // final sample
 
   // Print the recorded performance counter data
-  for ( unsigned int i = 1; i < total_iterations; ++i ) {
-    const auto& this_sample = samples.at( i );
-    const auto& last_sample = samples.at( i - 1 );
-    const auto& index_sample = samples.at( system_call_at );
-
-    const long int cum_instructions = this_sample.instructions - index_sample.instructions;
-    const auto instructions = this_sample.instructions - last_sample.instructions;
-    const auto cycles = this_sample.cycles - last_sample.cycles;
+  const auto index = samples.at( system_call_at ).post.instructions; // zero index = immediately after syscall
+  for ( unsigned int i = 0; i < total_iterations - 1; ++i ) {
+    if ( i == system_call_at ) {
+      cout << "# ";
+    }
+    const auto& sample = samples.at( i );
+    const auto relative_instruction_beginning = sample.pre.instructions - index;
+    const auto relative_instruction_ending = sample.post.instructions - index;
+    const auto instructions = sample.post.instructions - sample.pre.instructions;
+    const auto cycles = sample.post.cycles - sample.pre.cycles;
     const double ipc = double( instructions ) / double( cycles );
 
-    cout << cum_instructions << " " << ipc << "\n";
+    const auto box_middle = ( relative_instruction_beginning + relative_instruction_ending ) / 2;
+    const auto box_width = relative_instruction_ending - relative_instruction_beginning;
+
+    cout << box_middle << " " << ipc << " " << box_width << "\n";
   }
 
   return EXIT_SUCCESS;
