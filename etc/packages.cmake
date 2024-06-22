519f938e1329e5547b824b9b4d54ff468c7b42f5
Keith Winstein <keithw@cs.stanford.edu>
2024-06-21 20:28:54 -0700

    Sorting example

--- etc/packages.cmake
+++ etc/packages.cmake
@@ -1,6 +1,3 @@
 find_package(PkgConfig REQUIRED)
 pkg_check_modules(papi REQUIRED papi)
 include_directories(${papi_INCLUDE_DIRS})
-
-pkg_check_modules(Eigen REQUIRED IMPORTED_TARGET GLOBAL eigen3)
-include_directories(SYSTEM ${Eigen_INCLUDE_DIRS})
--- src/ipcfun.cc
+++ src/ipcfun.cc
@@ -1,6 +1,7 @@
 #include <sys/mman.h>
 
-#include <Eigen/Dense>
+#include <algorithm>
+#include <array>
 #include <cstdlib>
 #include <iostream>
 #include <vector>
@@ -9,8 +10,11 @@
 
 using namespace std;
 
-constexpr unsigned int total_iterations = 100000;
-constexpr unsigned int system_call_at = total_iterations / 2;
+constexpr size_t total_iterations = 100000;
+constexpr size_t system_call_at = total_iterations / 2;
+
+constexpr size_t num_random_vectors = 32;
+constexpr size_t random_vector_size = 16;
 
 struct SamplePair
 {
@@ -27,10 +31,17 @@ int main()
     throw runtime_error( "memfd_create" );
   }
 
-  // Initialize matrices for computation
-  vector<Eigen::Matrix<float, 7, 7>> matrices( 3 );
-  matrices[1].Random();
-  matrices[2].Random();
+  // Create "computation" task: sorting a random vector
+  array<array<char, random_vector_size>, num_random_vectors> data_to_sort, mutable_data_to_sort;
+  for ( auto& vec : data_to_sort ) {
+    for ( auto& val : vec ) {
+      val = rand();
+    }
+  }
+  vector<size_t> indices_to_sort( total_iterations );
+  for ( auto& index : indices_to_sort ) {
+    index = rand() % num_random_vectors;
+  }
 
   // Prevent CPU migration
   lock_to_CPU_zero();
@@ -48,32 +59,48 @@ int main()
       samples.at( i - 1 ).post = sample;
     }
 
-    if ( i == system_call_at ) { // do system call in this iteration (not printed)
-      CheckSystemCall( "pwrite", pwrite( fd, nullptr, 0, 0 ) );
-    } else { // otherwise, do computation and print stats
-      matrices[0] = matrices[1] * matrices[2];
+    if ( i == system_call_at ) { // do 1-byte pwrite system call in this iteration
+      char data_to_write = 42;
+      if ( sizeof( data_to_write )
+           != static_cast<size_t>(
+             CheckSystemCall( "pwrite", pwrite( fd, &data_to_write, sizeof( data_to_write ), 0 ) ) ) ) {
+        throw runtime_error( "short write" );
+      }
+    } else { // otherwise, do computation
+      const auto index = indices_to_sort[i];
+      mutable_data_to_sort[index] = data_to_sort[index];
+      sort( mutable_data_to_sort[index].begin(), mutable_data_to_sort[index].end() );
     }
   }
 
   samples.back().post = perf.read(); // final sample
 
   // Print the recorded performance counter data
-  const auto index = samples.at( system_call_at ).post.instructions; // zero index = immediately after syscall
+  const auto index_inst = samples.at( system_call_at ).post.instructions; // zero index = immediately after syscall
+  const auto index_cycle = samples.at( system_call_at ).post.cycles;      // zero index = immediately after syscall
   for ( unsigned int i = 0; i < total_iterations - 1; ++i ) {
     if ( i == system_call_at ) {
       cout << "# ";
     }
     const auto& sample = samples.at( i );
-    const auto relative_instruction_beginning = sample.pre.instructions - index;
-    const auto relative_instruction_ending = sample.post.instructions - index;
+    const auto relative_instruction_beginning = sample.pre.instructions - index_inst;
+    const auto relative_instruction_ending = sample.post.instructions - index_inst;
+
+    const auto relative_cycle_beginning = sample.pre.cycles - index_cycle;
+    const auto relative_cycle_ending = sample.post.cycles - index_cycle;
+
     const auto instructions = sample.post.instructions - sample.pre.instructions;
     const auto cycles = sample.post.cycles - sample.pre.cycles;
     const double ipc = double( instructions ) / double( cycles );
 
-    const auto box_middle = ( relative_instruction_beginning + relative_instruction_ending ) / 2;
-    const auto box_width = relative_instruction_ending - relative_instruction_beginning;
+    const auto inst_box_middle = ( relative_instruction_beginning + relative_instruction_ending ) / 2;
+    const auto inst_box_width = relative_instruction_ending - relative_instruction_beginning;
 
-    cout << box_middle << " " << ipc << " " << box_width << "\n";
+    const auto cycle_box_middle = ( relative_cycle_beginning + relative_cycle_ending ) / 2;
+    const auto cycle_box_width = relative_cycle_ending - relative_cycle_beginning;
+
+    cout << inst_box_middle << " " << ipc << " " << inst_box_width << " ";
+    cout << cycle_box_middle << " " << ipc << " " << cycle_box_width << "\n";
   }
 
   return EXIT_SUCCESS;
