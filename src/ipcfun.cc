c44dcd7864344fb32abf6b4a25c27437f12bbe59
Keith Winstein <keithw@cs.stanford.edu>
2024-06-21 21:20:54 -0700

    Allow command-line to choose syscall/nosyscall and branchy/matrix

--- src/ipcfun.cc
+++ src/ipcfun.cc
@@ -1,9 +1,11 @@
 #include <sys/mman.h>
 
+#include <Eigen/Dense>
 #include <algorithm>
 #include <array>
 #include <cstdlib>
 #include <iostream>
+#include <span>
 #include <vector>
 
 #include "support.hh"
@@ -21,11 +23,94 @@ struct SamplePair
   IPCCounter::Reading pre, post;
 };
 
-int main( int argc, char* argv[] [[maybe_unused]] )
+class Workload
+{
+  // Computation task with branches: sorting a random vector
+  array<array<char, random_vector_size>, num_random_vectors> data_to_sort {}, mutable_data_to_sort {};
+  vector<size_t> indices_to_sort {};
+
+  // Numerical computation: matrix multiplication
+  vector<Eigen::Matrix<float, 7, 7>> matrices { 3 };
+
+public:
+  Workload()
+  {
+    for ( auto& vec : data_to_sort ) {
+      for ( auto& val : vec ) {
+        val = rand();
+      }
+    }
+
+    indices_to_sort.resize( total_iterations );
+    for ( auto& index : indices_to_sort ) {
+      index = rand() % num_random_vectors;
+    }
+
+    matrices[1].Random();
+    matrices[2].Random();
+  }
+
+  void do_branchy_computation( size_t i )
+  {
+    const auto index = indices_to_sort.at( i );
+    mutable_data_to_sort[index] = data_to_sort[index];
+    sort( mutable_data_to_sort[index].begin(), mutable_data_to_sort[index].end() );
+  }
+
+  void do_matrix_computation() { matrices[0] = matrices[1] * matrices[2]; }
+};
+
+void trivial_memory_copy()
+{
+  volatile char x = 42;
+  volatile char y;
+  y = x;
+  x = y;
+}
+
+void usage_error( const span<char*>& args )
+{
+  cerr << "Usage: " << args[0] << " \"syscall\"/\"nosyscall\" \"branchy\"/\"matrix\"\n";
+  throw runtime_error( "invalid usage" );
+}
+
+tuple<bool, bool> process_arguments( const auto& args )
+{
+  if ( args.size() != 3 ) {
+    usage_error( args );
+  }
+
+  bool do_syscall;
+  if ( args[1] == "syscall"sv ) {
+    do_syscall = true;
+  } else if ( args[1] == "nosyscall"sv ) {
+    do_syscall = false;
+  } else {
+    usage_error( args );
+  }
+
+  bool branchy;
+  if ( args[2] == "branchy"sv ) {
+    branchy = true;
+  } else if ( args[2] == "matrix"sv ) {
+    branchy = false;
+  } else {
+    usage_error( args );
+  }
+
+  return tie( do_syscall, branchy );
+}
+
+int main( int argc, char* argv[] )
 {
   ios::sync_with_stdio( false );
 
-  const bool do_syscall = argc > 1;
+  // Parse arguments
+  if ( argc <= 0 ) {
+    abort();
+  }
+  auto args = span( argv, argc );
+  auto [do_syscall, branchy] = process_arguments( args );
 
   // Open dummy file
   int fd = memfd_create( "dummy", 0 );
@@ -33,22 +118,13 @@ int main( int argc, char* argv[] [[maybe_unused]] )
     throw runtime_error( "memfd_create" );
   }
 
-  // Create "computation" task: sorting a random vector
-  array<array<char, random_vector_size>, num_random_vectors> data_to_sort, mutable_data_to_sort;
-  for ( auto& vec : data_to_sort ) {
-    for ( auto& val : vec ) {
-      val = rand();
-    }
-  }
-  vector<size_t> indices_to_sort( total_iterations );
-  for ( auto& index : indices_to_sort ) {
-    index = rand() % num_random_vectors;
-  }
-
   // Prevent CPU migration
   lock_to_CPU_zero();
 
-  // Initialize IPC computation
+  // Initialize compute "workload"
+  Workload workload;
+
+  // Initialize monitoring of IPC (instructions per cycle)
   IPCCounter perf;
   vector<SamplePair> samples( total_iterations );
   perf.start();
@@ -61,17 +137,20 @@ int main( int argc, char* argv[] [[maybe_unused]] )
       samples.at( i - 1 ).post = sample;
     }
 
-    if ( i == system_call_at ) { // do 1-byte pwrite system call in this iteration
-      if ( do_syscall ) {
-        char data_to_write = 42;
-        if ( 1 != static_cast<size_t>( CheckSystemCall( "pwrite", pwrite( fd, &data_to_write, 1, 0 ) ) ) ) {
+    if ( i == system_call_at ) {
+      if ( do_syscall ) { // do 1-byte pwrite system call in this iteration
+        if ( 1 != CheckSystemCall( "pwrite", pwrite( fd, "x", 1, 0 ) ) ) {
           throw runtime_error( "short write" );
         }
+      } else { // copy one byte in user space (without a syscall)
+        trivial_memory_copy();
       }
-    } else { // otherwise, do computation
-      const auto index = indices_to_sort[i];
-      mutable_data_to_sort[index] = data_to_sort[index];
-      sort( mutable_data_to_sort[index].begin(), mutable_data_to_sort[index].end() );
+    } else { // otherwise, do some computation
+      if ( branchy ) {
+        workload.do_branchy_computation( i );
+      } else {
+        workload.do_matrix_computation();
+      }
     }
   }
 
@@ -103,12 +182,6 @@ int main( int argc, char* argv[] [[maybe_unused]] )
 
     cout << inst_box_middle << " " << ipc << " " << inst_box_width << " ";
     cout << cycle_box_middle << " " << ipc << " " << cycle_box_width << "\n";
-  }
-
-  if ( do_syscall ) {
-    cerr << "Did system call at iteration " << system_call_at << "\n";
-  } else {
-    cerr << "Did not do system call\n";
   }
 
   return EXIT_SUCCESS;
